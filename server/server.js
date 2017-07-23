f8f954345bdd069f2b202555e45342da8f97c5ad
BociucH\BociucH <dominikg@infobuzer.pl>
2017-07-23 14:47:55 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -26,6 +27,24 @@ app.get('/todos', (request, response) => {
         response.send({todos});
     }, (error) => {
         response.status(400).send(error);
+    });
+});
+
+app.get('/todos/:id', (request, response) => {
+    let id = request.params.id;
+
+    if (!ObjectId.isValid(id)) {
+        return response.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return response.status(404).send();
+        }
+
+        response.send({todo});
+    }).catch((error) => {
+        response.status(400).send();
     });
 });
 
